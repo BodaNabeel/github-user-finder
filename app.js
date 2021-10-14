b8df9d6c1396e237c6e2e0a52c70bbec90020839
Nabeel Boda <NeedToExplore01@gmail.com>
2021-10-14 16:27:18 +0530

    JS: Made container more dynamic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let person = {};
 const inputField = document.querySelector(".input-field");
 const submitBtn = document.querySelector(".btn-submit");
 
+const containerMain = document.querySelector(".container");
 let user_name = document.querySelector(".person-name");
 let user_dateOfJoin = document.querySelector(".person-dateOfJoin");
 let user_userName = document.querySelector(".person-userName");
@@ -26,7 +27,7 @@ const getInputData = function () {
 // 2. Updating UI
 const updateUI = function (data) {
   user_name.innerHTML = data.name;
-  user_dateOfJoin.innerHTML = data.created_at;
+  user_dateOfJoin.innerHTML = `Joined: ${data.created_at}`;
   user_userName = data.login;
   user_bio.innerHTML = data.bio;
   user_reposNo.innerHTML = data.public_repos;
@@ -38,21 +39,23 @@ const updateUI = function (data) {
   user_twitterHandle.innerHTML = data.twitter_username;
   user_profilePicture.src = data.avatar_url;
 };
+// 3. Fetch API
 async function fetchResults(userName) {
   const res = await fetch(`https://api.github.com/users/${userName}`);
   const json = await res.json();
   console.log(json);
   updateUI(json);
 }
-// Event Listeners
-submitBtn.addEventListener("click", () => {
+// 4. Things for addClicks
+const addEvent = function () {
   person.userName = getInputData();
   fetchResults(person.userName);
-});
+  containerMain.style.display = "block";
+};
+// Event Listeners
+submitBtn.addEventListener("click", () => addEvent());
 inputField.addEventListener("keypress", function (e) {
   if (e.keyCode === 13) {
-    person.userName = getInputData();
-    // fetchApi(person.userName);
-    fetchResults(person.userName);
+      addEvent()
   }
 });
