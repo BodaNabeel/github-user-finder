df20b2c8b69081dcb2fe412fd287cae4a3a5d105
fabcodingzest <fabrizvi786@gmail.com>
2021-10-18 00:52:47 +0530

    changing container layout and changing block to display

--- app.js
+++ app.js
@@ -37,47 +37,49 @@ const updateUI = function (data) {
             alt="Profile picture of user"
             class="container-header__left person-photo profile-picture"
           />
-          <div class="container-header__right">
-            <h2 class="person-name">${data.name}</h2>
-            <p class="person-dateOfJoin">Joined: ${dayjs(data.created_at).format('D-MMM-YYYY')}</p>
-            <p class="person-userName">@${data.login}</p>
-            <p class="person-bio">${data.bio}</p>
           </div>
-        </div>
-
-        <div class="container-detail">
-          <div class="detail">
-            <p class="detail-title">Repos</p>
-            <p class="detail-count repos-no">${data.public_repos}</p>
+          <div>
+            <div class="container-header__right">
+              <h2 class="person-name">${data.name}</h2>
+              <p class="person-dateOfJoin">Joined: ${dayjs(data.created_at).format('D-MMM-YYYY')}</p>
+              <p class="person-userName">@${data.login}</p>
+              <p class="person-bio">${data.bio}</p>
+            </div>
+            
+                    <div class="container-detail">
+            <div class="detail">
+              <p class="detail-title">Repos</p>
+              <p class="detail-count repos-no">${data.public_repos}</p>
+            </div>
+            <div class="detail">
+              <p class="detail-title">Followers</p>
+              <p class="detail-count followers-no">${data.followers}</p>
+            </div>
+            <div class="detail">
+              <p class="detail-title">Following</p>
+              <p class="detail-count following-no">${data.following}</p>
+            </div>
+                    </div>
+            
+                    <div class="container-info">
+            <div class="container-info__location container-info__flex">
+              <span class="material-icons-outlined icon"> room </span>
+              <p class="container-info__text location">${verifyValue(data.location)}</p>
+            </div>
+            <div class="container-info__location container-info__flex">
+              <i class="fab fa-twitter icon icon-twitter"></i>
+              <p class="container-info__text twitter-handle">${verifyValue(data.twitter_username)}</p>
+            </div>
+            <div class="container-info__location container-info__flex">
+              <span class="material-icons-outlined icon"> link </span>
+              <p class="container-info__text blog-link">${verifyValue(data.blog)}</p>
+            </div>
+            <div class="container-info__location container-info__flex">
+              <span class="material-icons-outlined icon"> location_city </span>
+              <p class="container-info__text status">${verifyValue(data.company)}</p>
+            </div>
+                    </div>
           </div>
-          <div class="detail">
-            <p class="detail-title">Followers</p>
-            <p class="detail-count followers-no">${data.followers}</p>
-          </div>
-          <div class="detail">
-            <p class="detail-title">Following</p>
-            <p class="detail-count following-no">${data.following}</p>
-          </div>
-        </div>
-
-        <div class="container-info">
-          <div class="container-info__location container-info__flex">
-            <span class="material-icons-outlined icon"> room </span>
-            <p class="container-info__text location">${verifyValue(data.location)}</p>
-          </div>
-          <div class="container-info__location container-info__flex">
-            <i class="fab fa-twitter icon icon-twitter"></i>
-            <p class="container-info__text twitter-handle">${verifyValue(data.twitter_username)}</p>
-          </div>
-          <div class="container-info__location container-info__flex">
-            <span class="material-icons-outlined icon"> link </span>
-            <p class="container-info__text blog-link">${verifyValue(data.blog)}</p>
-          </div>
-          <div class="container-info__location container-info__flex">
-            <span class="material-icons-outlined icon"> location_city </span>
-            <p class="container-info__text status">${verifyValue(data.company)}</p>
-          </div>
-        </div>
 `;
   containerMain.innerHTML = markUp;
 };
@@ -100,7 +102,7 @@ async function fetchResults(userName) {
 const addEvent = function () {
   person.userName = getInputData();
   fetchResults(person.userName);
-  containerMain.style.display = "block";
+  containerMain.style.display = "flex";
 };
 
 // 6. input field back to default
